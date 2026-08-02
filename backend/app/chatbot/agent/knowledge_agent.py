66792031a5cf2aac4c16721b2b472fc511aea7b4
NADEEM604539 <ndm604539@gmail.com>
2026-08-02 14:50:43 +0500

    created knowleget agent it will be handling the knowledge related control

---
+++ backend/app/chatbot/agent/knowledge_agent.py
@@ -0,0 +1,30 @@
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from app.chatbot.agent.tools import knowledge_tools
+
+
+llm = ChatOpenAI(
+    model="gpt-4.1-mini",
+    base_url="https://openai-rg-nadeem.openai.azure.com/openai/v1",
+)
+
+tools = knowledge_tools
+knowledge_agent = create_agent(
+    model=llm,
+    tools=tools
+)
+
+def gmail_Agent(user_id:int, query:str):
+    result = knowledge_agent.invoke(
+    {
+        "messages": [
+            {
+                "role": "user",
+                "content": f"u have to work and call tools according with the user_id as {user_id} and the query is {query}"
+            }
+        ]
+    }
+)
+    print(result)
+    return result["messages"][-1].content
+
