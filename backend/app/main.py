9fef9553c8f366f2fd7f51c918734cf3eea71df2
NADEEM604539 <ndm604539@gmail.com>
2026-08-04 14:35:24 +0500

    pushing changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://gmail-assistant-eight.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
