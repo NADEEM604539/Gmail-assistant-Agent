0c47002b2fe050f41a75e2fbf27b5702ff46fd24
NADEEM604539 <ndm604539@gmail.com>
2026-08-04 14:39:21 +0500

    removing additional / adding in cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://gmail-assistant-eight.vercel.app/"],
+    allow_origins=["https://gmail-assistant-eight.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
